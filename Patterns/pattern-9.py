cf101fdd72cd1635724428ebdd4f263d0f29e2d0
bhathiya <bhathiyabandara87@gmail.com>
2025-01-08 19:59:59 +0530

    Added Math problems

--- Patterns/pattern-9.py
+++ Patterns/pattern-9.py
@@ -1,5 +1,19 @@
 def pattern_9(n):
+    start = 1
     for i in range(n):
+        if i%2 == 0 : 
+                start = 0
+        else : start =1
+        for j in range(0,i):
+            print(start,end = ' ')
+            switch(start)
+    
+        print("")
+
+
         
+def switch(n):
+    if n == 0: return 1
+    else: return 0
 
 pattern_9(5)
